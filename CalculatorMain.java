00615a979ae095e90f4d1b6bc7f0e555f3cc1164
Varad <varad.manegopale28@gmail.com>
2026-02-24 17:09:09 +0530

    first commit

--- CalculatorMain.java
+++ CalculatorMain.java
@@ -1,6 +1,6 @@
 public class CalculatorMain{
 	public static void main(String[] args){
 	Calculator c1 = new Calculator();
-	System.out.println(c1.add(2,10));	
+	System.out.println("RESULT: "+c1.add(2,43));	
 }
 }
